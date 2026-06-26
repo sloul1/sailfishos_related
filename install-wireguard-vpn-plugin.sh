6ee1f0a6c4cd74aad677c8d562041dacfaa05442
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2026-06-26 12:34:52 +0000

    fix shell, run as admin

--- install-wireguard-vpn-plugin.sh
+++ install-wireguard-vpn-plugin.sh
@@ -4,15 +4,17 @@
 # HW: Sony Xperia 10 III
 # OS: Sailfish OS v.5.1.0.11 (Pispala)
 # (Android 13 as base before installing SFOS)
-
  
 # This script was created for enabling Mullvad VPN configuration
 # on Sailfish OS v.5.1.0.11 (Pispala)
 # The code functionality is copied from the SFOS Wiki:
-# https://sailfishos.wiki/books/network/page/installing-wireguard 
+# https://sailfishos.wiki/books/network/page/installing-wireguard
 
-#!/bin/bash
+# Run with administrative privileges:
+# devel-su ./install-wireguard-vpn-plugin.sh
 
-devel-su pkcon remove wireguard-go wireguard-tools  # for those whom previously installed those two packages in a previous setup or attempt
-devel-su pkcon install jolla-settings-networking-plugin-vpn-wireguard  # adds Wireguard among the proposed VPN in the settings
-devel-su systemctl restart connman  # restart the connexion manager to load the new Wireguard plugin
+#!/bin/sh
+
+pkcon remove wireguard-go wireguard-tools  # for those whom previously installed those two packages in a previous setup or attempt
+pkcon install jolla-settings-networking-plugin-vpn-wireguard  # adds Wireguard among the proposed VPN in the settings
+systemctl restart connman  # restart the connexion manager to load the new Wireguard plugin
